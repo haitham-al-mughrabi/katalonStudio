9f01627cf4d1a06b7cdcbe5f9d2fd686d8df76c4
haitham-mughrabi <haitham.almughrabi@optimumpartners.co>
2022-06-21 13:13:22 +0300

    added new test

---
+++ testDockerImage/Test Suites/testSuite1.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>testSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>585d28b1-50d2-4ce7-bf65-6027561878e1</testSuiteGuid>
+   <testCaseLink>
+      <guid>9fabb302-041a-4685-b5f0-4bc06a6adf77</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test-case-1</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
